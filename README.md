2fe11cdf4a4441eaf9f7db49e7f392f3eb1e8d22
lanx7 <lanx7@naver.com>
2016-01-11 20:37:43 +0900

    Add Initial Solution for PROBLEM 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python_Algorithm_Practice
 Python Algorithm Practice 
+
+1. BOGGLE GAME (EXHAUSTIVE SEARCH)
+
---
+++ boggle.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+def init_boggle(static=0):
+    if static == 0:
+        board = [['U','R','L','P','M'],
+                 ['X','P','R','E','T'],
+                 ['G','I','A','E','T'],
+                 ['X','T','N','Z','Y'],
+                 ['X','O','Q','R','S']]
+        return board
+    else:
+        board = np.ndarray((5,5))
+        return board
+dx = [-1, 0, 1, -1, 1, -1, 0, 1]
+dy = [-1, -1, -1, 0, 0, 1, 1, 1]
+level = 0
+answer = ''
+answer_xy = []
+
+def print_board(answer, answer_xy):
+    init_char = '*'
+    pboard = []
+    str = []
+    for i in range(5):
+        for j in range(5):
+            str.append(init_char)
+        pboard.append(str)
+        str = []
+
+    for idx, point in enumerate(answer_xy):
+        #print idx, point[0], point[1]
+        x = point[0]; y = point[1]
+        pboard[x][y] = answer[idx]
+
+    for line in pboard:
+        print line
+
+def hasword(x, y, word, level=0):
+    global answer
+    result = False
+    #print b[x][y]
+    if(x > 5 or y > 5):
+        return False
+
+    if(b[x][y] != word[level]):
+        return False
+
+    answer = answer + (b[x][y])
+    answer_xy.append((x,y))
+    if answer == word:
+        print 'Found answer: %s' % answer
+        #print 'Co', answer_xy
+        print_board(answer, answer_xy)
+        return True
+
+    for i in xrange(8):
+        result = hasword(x+dx[i], y+dy[i], word, level+1)
+        if result == True:
+            break
+
+
+    return result
+
+b = init_boggle()
+for i in b:
+    print i
+
+print hasword(0,1, 'RRE')
