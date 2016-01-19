50cb36d299d3ce3342af8c73c2a5c2667c60c45d
lanx7 <lanx7@naver.com>
2016-01-19 15:52:33 +0900

    picnic initial

--- picnic.py
+++ picnic.py
@@ -1,7 +1,33 @@
 
+def check_friends(picked, friends):
+    idx = 0
+    size_pairs = len(picked)/2
+    for i in range(size_pairs):
+        num1 = picked[i*2]
+        num2 = picked[i*2+1]
+        #print set([num1,num2])
+        if set([num1,num2]) not in friends:
+            return False
+
+    return True
+
+
+def pick(picked, total, friends, num_answer):
+    if len(picked) >= total:
+        if check_friends(picked, friends) == True:
+            print "Found Answer: ", picked
+            num_answer[0] = num_answer[0] + 1
+        return False
+
+    for i in range(total):
+        if i in picked:
+            continue
+        picked.append(i)
+        pick(picked, total,friends,num_answer)
+        picked.pop()
+
 
 num = 0
-
 with open('picnic_input.dat','r') as f:
     line = f.readline()
     num = int(line)
@@ -24,7 +50,9 @@ with open('picnic_input.dat','r') as f:
             friends.append(set([int(tokens2[j*2]),int(tokens2[j*2+1])]))
 
         print friends
+        answer = []
+        num_answer = [0]
+        pick(answer, tot, friends, num_answer)
+        print num_answer[0]
 
-def pick(picked):
-    if len(picked) > 4: 
-        return False
+
